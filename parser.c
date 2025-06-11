a5cfc4669f64cf4005df9fcee909c715922ad2dc
pablo <92185570+pabloduarte9679@users.noreply.github.com>
2025-06-11 00:15:52 -0600

    added token data structure

--- parser.c
+++ parser.c
@@ -21,9 +21,30 @@ typedef struct{
   void **records;
 }Table;
 
+typedef struct{
+  char *type;
+  char *data;
+}Token;
+
+Token *read_query(char *){
+  int c;
+  int state = 0;
+  char str[100];
+  while((c = query[i]) != '\0'){
+    if(c == ' ' || c == '\t' || c == '\n'){
+      state = 0;
+    }else if(state = 0){
+      state = 1;
+    }
+    if(state == 1 && c != '(' && c != ')' && c != ',' && c != ';'){
+      
+    }
+  }
+}
+
 int main(){
   char *datatypes[] = {"INTEGER", "TEXT", "REAL", "BLOB", "NULL"};
-  char *instructions[] = {"CREATE", "SELECT", "INSERT", "ALTER", "DROP", "DELETE", "UPDATE"};
+  char *instructions[] = {"CREATE", "SELECT", "INSERT", "ALTER", "DROP", "DELETE", "UPDATE", "FROM"};
   char *constraint[] = {"IF", "NOT", "TABLE", "EXISTS", "OR", "AND" "PRIMARY", "KEY", "FOREIGN", "NULL", "UNIQUE", "BY", "SET"};
   char *functions[] = {"AVG", "SUM", "COUNT", "MAX", "MIN"};
   char *filters[] = {"WHERE", "HAVING"};
@@ -31,7 +52,7 @@ int main(){
 
   char query[1024];
   fgets(query, 1024, stdin);
-
+  
   
   return 0;
 }
