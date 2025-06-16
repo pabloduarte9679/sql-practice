64cb4382c068b557f58349230776d6e219443fe7
pablo <92185570+pabloduarte9679@users.noreply.github.com>
2025-06-15 22:03:34 -0600

    bug detected

--- parser.c
+++ parser.c
@@ -26,33 +26,44 @@ typedef struct{
   char *data;
 }Token;
 
-Token *read_query(char *){
-  int c;
-  int state = 0;
-  char str[100];
-  while((c = query[i]) != '\0'){
-    if(c == ' ' || c == '\t' || c == '\n'){
-      state = 0;
-    }else if(state = 0){
-      state = 1;
-    }
-    if(state == 1 && c != '(' && c != ')' && c != ',' && c != ';'){
-      
-    }
-  }
-}
+Token *read_query(char *query);
 
 int main(){
   char *datatypes[] = {"INTEGER", "TEXT", "REAL", "BLOB", "NULL"};
-  char *instructions[] = {"CREATE", "SELECT", "INSERT", "ALTER", "DROP", "DELETE", "UPDATE", "FROM"};
+  char *instructions[] = {"CREATE", "SELECT", "INSERT", "ALTER", "DROP", "DELETE", "UPDATE", "FROM", "ORDER"};
   char *constraint[] = {"IF", "NOT", "TABLE", "EXISTS", "OR", "AND" "PRIMARY", "KEY", "FOREIGN", "NULL", "UNIQUE", "BY", "SET"};
   char *functions[] = {"AVG", "SUM", "COUNT", "MAX", "MIN"};
   char *filters[] = {"WHERE", "HAVING"};
   char operators[] = {'+', '-', '*', '/', '!', '='};
 
   char query[1024];
-  fgets(query, 1024, stdin);
+  fgets(query, 10, stdin);
+  printf("lol\n");
+  query[10] = '\0';
+
   
+  Token *tokens = read_query(query);
   
   return 0;
+}
+
+
+Token *read_query(char *query){
+  int c;
+  int state = 0;
+  char str[100];
+  Token tokens[100];
+  int index = 0, local_index = 0;
+  while((c = query[local_index]) != '\0'){
+    if(c == ' ' || c == '\t' || c == '\n'){
+      state = 0;
+    }else if(state = 0){
+      state = 1;
+    }
+    if(state == 1 && c != '(' && c != ')' && c != ',' && c != ';'){
+      tokens[index].data[local_index++] = c;
+    }
+  }
+  printf("%s", tokens[index].data);
+  return NULL;
 }
