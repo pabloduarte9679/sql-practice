7b9ae6599d0eea9012193d2f724b6b7896648485
pabloduarte9679 <elierduarte344@gmai.com>
2025-06-25 01:09:13 -0600

    added types definitions

--- lexer.h
+++ lexer.h
@@ -1,6 +1,18 @@
 #ifndef LEXER_H
 #define LEXER_H
 
+#define DATATYPE          0 
+#define STATEMENT         1
+#define CONSTRAINT        2
+#define FUNCTION          3
+#define FILTER            4
+#define OPERATOR          5
+#define OPEN_PARENTHESES  6
+#define CLOSE_PARENTHESES 7
+#define COMMA             8
+#define SEMICOLON         9
+#define INDENTIFIER      10
+
 typedef struct{
   char type;
   char data[100];
