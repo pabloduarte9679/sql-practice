114d8895711fd21b23dff73f44efa4ff79c70ed4
pabloduarte9679 <elierduarte344@gmai.com>
2025-06-26 16:07:49 -0600

    added more types definitions

--- lexer.h
+++ lexer.h
@@ -12,9 +12,44 @@
 #define COMMA             8
 #define SEMICOLON         9
 #define INDENTIFIER      10
+#define INTEGER          11
+#define TEXT             12
+#define REAL 	    	 13
+#define BLOB             14
+#define NULL             15
+#define CREATE           16
+#define SELECT           17
+#define INSERT           18
+#define ALTER            19
+#define DROP             20
+#define DELETE           21
+#define UPDATE           22
+#define FROM             23
+#define ORDER            24
+#define IF               25
+#define NOT              26
+#define TABLE            27
+#define EXISTS           28
+#define OR               29
+#define AND              30
+#define PRIMARY          31
+#define KEY              32
+#define FOREIGN          33
+#define NULL_C           34
+#define UNIQUE           35
+#define BY               36
+#define SET              37
+#define WHERE            38
+#define HAVING           39
+#define PLUS             40
+#define MINUS            41
+#define TIMES            42
+#define DIVSION          43
+#define NOT_OP           44
+#define EQUAL            45
 
 typedef struct{
-  char type;
+  int type;
   char data[100];
 }Token;
 
