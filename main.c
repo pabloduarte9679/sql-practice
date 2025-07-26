b518c6aa0cf511e6b06c8f62eaa786cc34dd3dd2
pabloduarte9679 <elierduarte344@gmail.com>
2025-07-26 01:22:00 -0600

    now main links every other file in the program

--- main.c
+++ main.c
@@ -1,1 +1,23 @@
+#include "lexer.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+int main(){
+  char query[1000];
+  fgets(query, 1000, stdin);
+  for(int i = 0; i < 1000; i++){
+    if(query[i] == '\n'){
+      query[i] = '\0';
+      break;
+    }
+  }
+  int count;
+  Token *tokens = lexer2(query, &count);
+  for(int i = 0; i < count; i++){
+    printf("tokens[%d] = %s, type = %d\n", i, tokens[i].value, tokens[i].type);
+  }
+  free(tokens);
+  return 0;
+}
