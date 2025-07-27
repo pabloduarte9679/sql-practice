a2ccf7fd8eb8c363f219c50d37dd53043097dba0
pabloduarte9679 <elierduarte344@gmail.com>
2025-07-27 01:47:04 -0600

    added create table statement data type and constraint type

--- parser.h
+++ parser.h
@@ -30,6 +30,20 @@ typedef struct{
   Token *tokens;
 }Stmn;
 
+typedef struct{
+  int type;
+  char *name;
+  Column *cons_column;
+  Table *ref_table;
+}Constraint;
+
+typedef struct{
+  char *name;
+  int ifnotexists;
+  Column *colum_defs;
+  Constraint *cons_defs;
+}Create_table_stmt;
+
 Stmn *parser(Token *token_list);
 Stmn *parse_create_table(Token *token_list);
 void panic(char *err);
