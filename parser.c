c582fd525754ad55253c9e79512ca9ff5189f7d1
pabloduarte9679 <elierduarte344@gmail.com>
2025-07-27 01:41:11 -0600

    fixed indentation

--- parser.c
+++ parser.c
@@ -16,37 +16,36 @@ int main(){
 }
 
 Stmn *parser(Token *token_list){
- while(token_list->type != SEMI){
-   switch(token_list->type){
-     case CREATE:
-       return parse_create_table(token_list);
-       break;
-     default:
-       printf("unknown statementn\n");
-   }
+  while(token_list->type != SEMI){
+    switch(token_list->type){
+      case CREATE:
+        return parse_create_table(token_list);
+        break;
+      default:
+        printf("unknown statementn\n");
+    }
 
-   token_list++;
- }
+    token_list++;
+  }
 }
 
 Stmn *parse_create_table(Token *token_list){
   token_list++;
-	if(token_list->type == TABLE){
+  if(token_list->type == TABLE){
     token_list++;
-	}else{
+  }else{
     printf("UNKNOWN CREATE TYPE\n");
-		return NULL;
-	}
-	if(token_list->type == LP)  token_list++;
-	else if (token_list->type == IF && (token_list+1)->type == NOT && (token_list+2)->type == EXISTS) token_list += 2;
-	else panic("Missing (");
+    return NULL;
+  }
 
-	if(token_list->type == IDENTIFIER){}
+  if(token_list->type == LP)  token_list++;
+  else if (token_list->type == IF && (token_list+1)->type == NOT && (token_list+2)->type == EXISTS) token_list += 2;
+  else panic("Missing (");
 
-
+  if(token_list->type == IDENTIFIER){}
 }
 
 void panic(char *err){
   puts(err);
-	exit(1);
+  exit(1);
 }
